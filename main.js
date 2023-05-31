ac2c05ee6981609c06bc89e5cd80f53dbe6e717f
AndreyUA <infernalsteel666@gmail.com>
2023-05-31 22:12:22 +0300

    empty scene with camera and responsive view

--- main.js
+++ main.js
@@ -4,3 +4,23 @@ import * as THREE from "three";
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1_000
+);
+
+const animate = () => {
+  renderer.render(scene, camera);
+};
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
